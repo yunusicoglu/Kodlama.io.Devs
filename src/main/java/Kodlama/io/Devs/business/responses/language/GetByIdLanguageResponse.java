7019b227c7a0693133d269ac8067c49c319569b8
Yunus İçoğlu <yunusicoglu@gmail.com>
2022-12-10 12:19:10 +0300

    10.11.22

---
+++ src/main/java/Kodlama/io/Devs/business/responses/language/GetByIdLanguageResponse.java
@@ -0,0 +1,13 @@
+package Kodlama.io.Devs.business.responses.language;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetByIdLanguageResponse {
+	private int id;
+	private String name;
+}
